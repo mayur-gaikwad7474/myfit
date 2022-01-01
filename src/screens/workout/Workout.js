4bcf50f272b0dc00c04e53811465898b280b200e
Mayur Gaikwad <mayur.gaikward@aitrade.ai>
2022-01-01 13:43:14 +0530

    added new data'

--- src/screens/workout/Workout.js
+++ src/screens/workout/Workout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Workout.css'
 import Topbar from '../../components/topbar/Topbar'
 import Cross from '../../assets/Cross.svg'
@@ -13,119 +13,121 @@ import Exercise7 from '../../assets/exercise7.png'
 import Exercise8 from '../../assets/exercise8.png'
 
 
-const Workout = () => {
-    return (
-        <React.Fragment>
-            <Topbar />
-            <div className="workout-container">
-                <div className="main-card">
-                    <div className="card">
-                        <div className="top-section">
-                            <span></span>
-                            <span className="input-wrapper">
-                                <input type="text" placeholder="Jumping Jack" />
-                                <button>Search</button>
-                            </span>
-                            <img src={Cross} alt="" />
-                        </div>
+class Workout extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Topbar />
+                <div className="workout-container">
+                    <div className="main-card">
+                        <div className="card">
+                            <div className="top-section">
+                                <span></span>
+                                <span className="input-wrapper">
+                                    <input type="text" placeholder="Jumping Jack" />
+                                    <button>Search</button>
+                                </span>
+                                <img src={Cross} alt="" />
+                            </div>
 
-                        <div className="video-section">
-                            <img className="video" alt="" src="https://images.unsplash.com/photo-1546483875-ad9014c88eba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGZpdG5lc3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
-                            <div className="info-container">
-                                <div className="exercise-info">
+                            <div className="video-section">
+                                <img className="video" alt="" src="https://images.unsplash.com/photo-1546483875-ad9014c88eba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGZpdG5lc3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
+                                <div className="info-container">
+                                    <div className="exercise-info">
+                                        <span>Name</span>
+                                        <span>Reps</span>
+                                        <span>Calorie Burn</span>
+                                    </div>
+                                    <div className="exercise-info-todo">
+                                        <span>Jumping Jack</span>
+                                        <span>05</span>
+                                        <span>30 Burn</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="button-section">
+                                <button className="button-container">
+                                    <button>+</button>
+                                    <span>Add Custom Exercise</span>
+                                </button>
+
+                                <div className="row">
+                                    <img src={Filter} alt="" />
                                     <span>Name</span>
-                                    <span>Reps</span>
-                                    <span>Calorie Burn</span>
-                                </div>
-                                <div className="exercise-info-todo">
-                                    <span>Jumping Jack</span>
-                                    <span>05</span>
-                                    <span>30 Burn</span>
                                 </div>
                             </div>
-                        </div>
-
-                        <div className="button-section">
-                            <button className="button-container">
-                                <button>+</button>
-                                <span>Add Custom Exercise</span>
-                            </button>
-
-                            <div className="row">
-                                <img src={Filter} alt="" />
-                                <span>Name</span>
-                            </div>
-                        </div>
-                        <div className="grid">
-                            <div className="image-card">
-                                <img src={Exercise1} alt="" />
-                                <div className="row">
-                                    <span>Push - Ups</span>
-                                    <button><span>+</span></button>
+                            <div className="grid">
+                                <div className="image-card">
+                                    <img src={Exercise1} alt="" />
+                                    <div className="row">
+                                        <span>Push - Ups</span>
+                                        <button><span>+</span></button>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className="image-card">
-                                <img src={Exercise2} alt="" />
-                                <div className="row">
-                                    <span>Push - Ups</span>
-                                    <button><span>+</span></button>
+                                <div className="image-card">
+                                    <img src={Exercise2} alt="" />
+                                    <div className="row">
+                                        <span>Push - Ups</span>
+                                        <button><span>+</span></button>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className="image-card">
-                                <img src={Exercise3} alt="" />
-                                <div className="row">
-                                    <span>Push - Ups</span>
-                                    <button><span>+</span></button>
+                                <div className="image-card">
+                                    <img src={Exercise3} alt="" />
+                                    <div className="row">
+                                        <span>Push - Ups</span>
+                                        <button><span>+</span></button>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className="image-card">
-                                <img src={Exercise4} alt="" />
-                                <div className="row">
-                                    <span>Push - Ups</span>
-                                    <button><span>+</span></button>
+                                <div className="image-card">
+                                    <img src={Exercise4} alt="" />
+                                    <div className="row">
+                                        <span>Push - Ups</span>
+                                        <button><span>+</span></button>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className="image-card">
-                                <img src={Exercise5} alt="" />
-                                <div className="row">
-                                    <span>Push - Ups</span>
-                                    <button><span>+</span></button>
+                                <div className="image-card">
+                                    <img src={Exercise5} alt="" />
+                                    <div className="row">
+                                        <span>Push - Ups</span>
+                                        <button><span>+</span></button>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className="image-card">
-                                <img src={Exercise6} alt="" />
-                                <div className="row">
-                                    <span>Push - Ups</span>
-                                    <button><span>+</span></button>
+                                <div className="image-card">
+                                    <img src={Exercise6} alt="" />
+                                    <div className="row">
+                                        <span>Push - Ups</span>
+                                        <button><span>+</span></button>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className="image-card">
-                                <img src={Exercise7} alt="" />
-                                <div className="row">
-                                    <span>Push - Ups</span>
-                                    <button><span>+</span></button>
+                                <div className="image-card">
+                                    <img src={Exercise7} alt="" />
+                                    <div className="row">
+                                        <span>Push - Ups</span>
+                                        <button><span>+</span></button>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className="image-card">
-                                <img src={Exercise8} alt="" />
-                                <div className="row">
-                                    <span>Push - Ups</span>
-                                    <button><span>+</span></button>
+                                <div className="image-card">
+                                    <img src={Exercise8} alt="" />
+                                    <div className="row">
+                                        <span>Push - Ups</span>
+                                        <button><span>+</span></button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </React.Fragment>
-    )
+            </React.Fragment>
+        )
+    }
 }
 
 export default Workout
