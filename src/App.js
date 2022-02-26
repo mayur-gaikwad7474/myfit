20dc97ee8360fe8204faa19a4959eab360c2de5c
Malav Warke <malav2202@gmail.com>
2022-02-26 12:29:36 +0530

    added new screen

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Addnewworkout from './screens/addnewworkout/Addnewworkout'
-import Addworkout from './screens/addworkout/Addworkout'
-import Popup from './screens/popup/Popup'
-import Viewworkout from './screens/viewworkout/Viewworkout'
-import Workout from './screens/workout/Workout'
+import Screen1 from './screens/screen1/Screen1'
+import Screen2 from './screens/screen2/Screen2'
+import Screen3 from './screens/screen3/Screen3'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Workout />} />
-        <Route path="/page2" element={<Viewworkout />} />
-        <Route path="/page3" element={<Addworkout />} />
-        <Route path="/page4" element={<Addnewworkout />} />
-        <Route path="/page5" element={<Popup />} />
+         <Route path="/" element={<Screen1 />} />
+         <Route path="/2" element={<Screen2 />} />
+         <Route path="/3" element={<Screen3 />} />
       </Routes>
     </BrowserRouter>
   )
